07274a4ba66c722424ab4ebca0e4f56885f05750
HectorxH <HectorPLJ@gmail.com>
2021-07-01 21:14:05 -0400

    Small run script

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [[ $1 == "dev" ]]; then
+    echo "Running on Development mode!"
+    hug -f app.py
+elif [[ $1 == "prod" ]]; then
+    echo "Running on Production mode."
+    gunicorn app:__hug_wsgi__
+fi
+
+echo "Run with 'dev' or 'prod'."
