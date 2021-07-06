afa5587bf43dec250f47c7a0e47d8c4da2be16a4
HectorxH <HectorPLJ@gmail.com>
2021-07-05 20:04:48 -0400

    API pulenta

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 import hug
 
+BD_capacitaciones = {}
+BD_capacitados = {}
+BD_recursos = {}
 
-@hug.get('/')
-def hello(request):
-    print(request)
+
+@hug.get('/habilitacion/capacitaciones')
+def get_capacitaciones():
     return {"Hello World": 1}
+
+
+@hug.get('/habilitacion/capacitados')
+def get_todos_capacitados(id_capacitacion: hug.types.text):
+    return BD_capacitados[id_capacitacion]
+
+
+@hug.get('/habilitacion/capacitaciones/{id_capacitado}')
+def get_un_capacitado(id_capacitado: hug.type.text):
+    return {"Hello World": 1}
+
+
+@hug.get('/habilitacion/recursos/{id_capcitacion}')
+def get_recursos(id_capacitacion: hug.type.text):
+    return BD_recursos[id_capacitacion]
+
+
+@hug.get('/habilitacion/{id_persona}{id_capacitacion}')
+def check_capacitacion(id_persona: hug.types.text, id_capacitacion: hug.types.text):
+    return "😎"
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ $1 == "dev" ]]; then
     echo "Running on Development mode!"
     hug -f app.py
 elif [[ $1 == "prod" ]]; then
     echo "Running on Production mode."
-    gunicorn app:__hug_wsgi__
+    gunicorn --bind 0.0.0.0:8000 app:__hug_wsgi__
 fi
 
 echo "Run with 'dev' or 'prod'."
